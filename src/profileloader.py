aafd141f2ef2705526b94da9d06690a01abb4322
viktorcus <carolynseglem@gmail.com>
2026-04-20 09:13:39 +0200

    Added a new method to apply load and generation profiles to a pandapower net, treating each bus as a collection of one or more of these profiles.

--- src/profileloader.py
+++ src/profileloader.py
@@ -1,53 +1,89 @@
 import pandas as pd
 from pandapower.control import ConstControl
 from pandapower.timeseries.data_sources.frame_data import DFData
+from typing import Literal
+import json
+import numpy as np
 
+_NET_ELEMENT = Literal["load", "gen"]
 
-def datasource_loads(net):
+def net_element(net, net_element_: _NET_ELEMENT):
+    return net.load if net_element_ == "load" else net.gen
+
+def csv_to_net(net, net_element_ : _NET_ELEMENT = "load"):
     """
-        Handles differences between the number of load profiles given and the number of loads in a net.
-        Splits the power from the load profiles as equally as across the available loads.
+        Provided a set of CSVs representing a timeseries of load data, parses these and allocates them evenly 
+        across a net with a variable number of loads.
+        Makes the assumption that only one instance of each load profile is being processed in the network, and 
+        that there is no preference to which node the load profile belongs to.
     """
-    load_profile = pd.read_csv('../data/load_profiles/csv/load_profiles_joined.csv')
-    num_profiles = len(load_profile.columns) - 1
-    num_loads = len(net.load)
+    profile_df = pd.read_csv(f'../data/{net_element_}_profiles/csv/{net_element_}_profiles_joined.csv')
+    num_profiles = len(profile_df.columns) - 1
+    num_spaces = len(net_element(net, net_element))
 
 
     # insufficient load profiles
-    if num_loads < num_profiles:
-        raise IndexError(f'Load profiles ({num_profiles}) exceed number of loads in net ({num_loads})')
+    if num_spaces < num_profiles:
+        raise IndexError(f'{net_element_} profiles ({num_profiles}) exceed number of spaces available in net ({num_spaces})')
     
     # number of loads in the net is a multiple of the number of load profiles:
     # loads can all be split the same number of ways
-    elif num_loads % num_profiles == 0:
-        allocations = num_loads / num_profiles
+    elif num_spaces % num_profiles == 0:
+        allocations = num_spaces / num_profiles
 
         for i in range(1, num_profiles):
-            pd.to_numeric(load_profile.iloc[:,i])
-            load_profile.iloc[:,i] = load_profile.iloc[:,i] / allocations
+            pd.to_numeric(profile_df.iloc[:,i])
+            profile_df.iloc[:,i] = profile_df.iloc[:,i] / allocations
 
             for p in range(allocations):
-                load_profile[f'{load_profile.columns.values[i]} {p+2}'] = load_profile.iloc[:,i]
+                profile_df[f'{profile_df.columns.values[i]} {p+2}'] = profile_df.iloc[:,i]
 
     # number of loads in the net is not a multiple of the number of load profiles:
     # a modules m number of load profiles will be divided by a factor of the whole number n + 1, the rest are divided by a factor of n
     else:
-        for i in range(1, (num_loads % num_profiles) + 1):
-            allocations = num_loads // num_profiles
-            pd.to_numeric(load_profile.iloc[:,i])
-            load_profile.iloc[:,i] = (load_profile.iloc[:,i] / float(allocations))
+        for i in range(1, (num_spaces % num_profiles) + 1):
+            allocations = num_spaces // num_profiles
+            pd.to_numeric(profile_df.iloc[:,i])
+            profile_df.iloc[:,i] = (profile_df.iloc[:,i] / float(allocations))
 
             for p in range(allocations):
-                load_profile[f'{load_profile.columns.values[i]} {p+2}'] = load_profile.iloc[:,i]
+                profile_df[f'{profile_df.columns.values[i]} {p+2}'] = profile_df.iloc[:,i]
 
-        for i in range((num_loads % num_profiles) + 1, num_profiles + 1):
-            allocations = num_loads // num_profiles -1
-            pd.to_numeric(load_profile.iloc[:,i])
-            load_profile.iloc[:,i] = load_profile.iloc[:,i] / allocations
+        for i in range((num_spaces % num_profiles) + 1, num_profiles + 1):
+            allocations = num_spaces // num_profiles -1
+            pd.to_numeric(profile_df.iloc[:,i])
+            profile_df.iloc[:,i] = profile_df.iloc[:,i] / allocations
 
             for p in range(allocations):
-                load_profile[f'{load_profile.columns.values[i]} {p+2}'] = load_profile.iloc[:,i]
+                profile_df[f'{profile_df.columns.values[i]} {p+2}'] = profile_df.iloc[:,i]
 
-    print(load_profile.head())
+    ds = DFData(profile_df.iloc[:,1:])
+
+    return ConstControl(net, element=net_element_, variable='p_mw', data_source=ds, 
+                              element_index=net_element(net, net_element).index, profile_name=net_element(net, net_element).index)
+
+def json_to_net(net, net_element_: _NET_ELEMENT = "load"):
+    '''
+        Reads a JSON file that describes assignments of a set of load/gen profiles to a set of loads/gens in a net. 
+        Each element may be a collection of one or more of these profiles, which each may be counted multiple 
+        times (e.g., one load may contain one school and twenty houses). 
+    '''
+    profile_df = pd.read_csv(f'../data/{net_element_}_profiles/csv/{net_element_}_profiles_joined.csv')
+
+    with open(f'../data/{net_element_}_profiles/{net_element_}_allocations.json') as f:
+        data = json.load(f)
+        loading_df = pd.DataFrame(0, index=np.arange(35401), 
+                                  columns=np.arange(len(net_element(net, net_element))))
+            
+        for idx in range(len(data["data"])):
+            d = data["data"][idx]
+
+            for profile_name, profile_count in d["profiles"].items():
+                loading_df[idx] += profile_df[profile_name] * profile_count
+
+    ds = DFData(loading_df)
     
+    return ConstControl(net, element=net_element_, variable='p_mw', data_source=ds, 
+                              element_index=net_element(net, net_element).index, profile_name=net_element(net, net_element).index)
+
 
