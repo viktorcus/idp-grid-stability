38e7d67a6411a338673683d574174b02bf34890b
viktorcus <carolynseglem@gmail.com>
2026-05-16 18:54:17 +0200

    Minor refactoring of limits.py to better produce condensed results in timeseries.

--- src/tools/limits.py
+++ src/tools/limits.py
@@ -6,35 +6,35 @@ Version: 1.0
 Description: Contains a custom set of functions for testing a pandapower net's results against each components' defined limits.
 """
 
-def bus_vm_pu_limits(net):
+def bus_vm_pu_limits(net, verbose: bool = False):
+    err_list = []
     try:
-        err_found = False
         for i in range(len(net.bus)):
             bus = net.bus.iloc[i]
             res = net.res_bus.iloc[i]
             
             if res.vm_pu-bus.min_vm_pu < -0.0001: 
-                print(f'Bus #{bus.name}: Result {round(res.vm_pu,4)} V p.u. is less than minimum of {bus.min_vm_pu} V p.u.')
-                err_found = True
+                if verbose: print(f'Bus #{bus.name}: Result {round(res.vm_pu,4)} V p.u. is less than minimum of {bus.min_vm_pu} V p.u.')
+                err_list.append(bus.name)
             elif res.vm_pu-bus.max_vm_pu > 0.0001:
-                print(f'Bus #{bus.name}: Result {round(res.vm_pu,4)} V p.u. is greater than maximum of {bus.max_vm_pu} V p.u.')
-                err_found = True
+                if verbose: print(f'Bus #{bus.name}: Result {round(res.vm_pu,4)} V p.u. is greater than maximum of {bus.max_vm_pu} V p.u.')
+                err_list.append(bus.name)
             
-        if not err_found: print("All buses within limits")
+        if len(err_list) == 0: print("All buses within limits") 
     except AttributeError:
         print("Power flow has not yet been run - no results to analyze")
 
-def line_loading_limits(net):
+def line_loading_limits(net, verbose: bool = False):
+    err_list = []
     try:
-        err_found = False
         for i in range(len(net.line)):
             line = net.line.iloc[i]
             res = net.res_line.iloc[i]
             
             if res.loading_percent-line.max_loading_percent > 0.0001:
-                print(f'Line #{line.name}: Result {round(res.loading_percent,4)}% loading is greater than maximum of {line.max_loading_percent}% loading')
-                err_found = True
+                if verbose: print(f'Line #{line.name}: Result {round(res.loading_percent,4)}% loading is greater than maximum of {line.max_loading_percent}% loading')
+                err_list.append(line.name)
             
-        if not err_found: print("All line loading within limits")
+        if len(err_list) == 0: print("All line loading within limits")
     except AttributeError:
         print("Power flow has not yet been run - no results to analyze")
