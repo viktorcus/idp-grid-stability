107c7035e0f2b5b391d749139ec0984d2a31a22c
viktorcus <carolynseglem@gmail.com>
2026-06-10 09:16:29 +0200

    Converted problem/algorithm to Mixed Variable GA. Fixed more checkpointing issues.

--- src/ga_optimizer.py
+++ src/ga_optimizer.py
@@ -1,43 +1,51 @@
 import numpy as np
+import multiprocessing
+import sys
+import dill
+import os
+import traceback
+
 from pandapower.run import runpp
 from pandapower.timeseries.run_time_series import run_timeseries
 from pandapower.networks.power_system_test_cases import case30
 from pandapower.create import create_storage
+
 import profileloader as pl
+
 from control.hydrogen import Hydrogen
 from control.battery import Battery
 from control.timestep import TimeStepTracker
-from tools.ga.problem import GridPlanningProblem
+
 from tools.test_runner import *
+from tools.ga.problem import GridPlanningProblem
+from tools.ga.solution import Solution
+
 from pymoo.algorithms.soo.nonconvex.ga import GA
 from pymoo.optimize import minimize
-from tools.ga.solution import Solution
-import multiprocessing
-from pymoo.parallelization.starmap import StarmapParallelization
-from pymoo.operators.sampling.rnd import FloatRandomSampling
-import sys
-import dill
-import traceback
 from pymoo.core.callback import Callback
+from pymoo.core.mixed import MixedVariableGA
 from pymoo.termination import get_termination
+from pymoo.parallelization.starmap import StarmapParallelization
+
 
 # callback class for algorithm checkpoints
 class CheckpointCallback(Callback):
 
     def notify(self, algorithm):
+        runner = algorithm.problem.elementwise_runner
+        algorithm.problem.elementwise_runner = None
 
-        with open("checkpoint.pkl", "wb") as f:
-            dill.dump({"algorithm": algorithm}, f)
+        try:
+            with open("checkpoint.pkl", "wb") as f:
+                dill.dump({"algorithm": algorithm}, f)
+        finally:
+            algorithm.problem.elementwise_runner = runner
         
 
 # define globals
 test_date = None
 
 max_discrepancy = 0
-bus_violation_cost = {}
-line_violation_cost = {}
-ext_grid_penalty_cost = {}
-ext_grid_prices_cost = {}
 
 monthly_profiles = {}
 iteration_counter = 0
@@ -48,11 +56,7 @@ def init_run(date=None):
         Reset net before a timeseries run, with default case 30 values and timeseries data imported from data files
     """
     # reset global variables
-    global bus_violation_cost, line_violation_cost, ext_grid_penalty_cost, ext_grid_prices_cost, test_date
-    bus_violation_cost = {}
-    line_violation_cost = {}
-    ext_grid_penalty_cost = {}
-    ext_grid_prices_cost = {}
+    global test_date
     test_date = date
 
     # reset net
@@ -61,10 +65,11 @@ def init_run(date=None):
      # import profiles for timeseries data
     tariff_control = pl.json_to_net_generic(net, "poly_cost", date=date)
     load_control = pl.json_to_net_generic(net, "load", date=date)
-    net.load["q_mvar"] = 0      # not accounting for this for the time being
     gen_control = pl.json_to_net_pv(net, date=date)
     hydro_control = pl.json_to_net_hydro(net, date=date)
     tracker_control = TimeStepTracker(net)
+
+    net.load["q_mvar"] = 0      # not accounting for this for the time being
     net.ext_grid["controllable"] = False
     net.gen["controllable"] = False
 
@@ -74,6 +79,8 @@ def deploy_net_runner(solution: Solution, month=None, date=None):
     """ 
     Dispatches the storage options according to the current GA optimizer solution, and runs the timeseries
     """
+    global monthly_profiles
+
     net = init_run(date)
     if month is not None:
         net = extract_monthly_profile(net, monthly_profiles, month)
@@ -157,8 +164,27 @@ def deploy_net_runner(solution: Solution, month=None, date=None):
                                     num_tanks=solution.h2_num_tanks2)
         net.poly_cost = storage_control.create_cost_element(net)
 
-    run_timeseries(net, continue_on_divergence=False, max_iteration=40, run=timeseries_runner, verbose=True, time_steps=range(0,96))  
-    return sum(bus_violation_cost.values()) + sum(line_violation_cost.values()) + sum(ext_grid_penalty_cost.values()) + sum(ext_grid_prices_cost.values())
+    acc = {
+        "bus": 0.0,
+        "line": 0.0,
+        "grid_penalty": 0.0,
+        "grid_price": 0.0
+    }
+
+    def local_runner(net, **kwargs):
+        timeseries_runner(net, acc)
+
+    run_timeseries(net, continue_on_divergence=False, max_iteration=40, run=local_runner, verbose=True, time_steps=range(0,96))  
+    total = (
+        acc["bus"]
+        + acc["line"]
+        + acc["grid_penalty"]
+        + acc["grid_price"]
+        + grid_component_prices(net)
+    )
+    print(f'bus err: {acc["bus"]}   line err: {acc["line"]}  grid reliance penalty:  {acc["grid_penalty"]}   grid prices: {acc["grid_price"]}   components: {grid_component_prices(net)}   total cost {total}')    
+    
+    return total
 
 def bus_violations(net):
     """
@@ -181,14 +207,16 @@ def line_violations(net):
     loading = net.res_line.loading_percent.values
     max_loading = net.line.max_loading_percent.values
 
-    line_violation = np.maximum(0.0, loading - max_loading) / 100
+    line_violation = np.maximum(0.0, loading - max_loading)
     return np.sum(line_violation ** 2)
 
 def grid_penalty(net):
     """
     Returns squared error representing the amount by which the system relies on selling/buying power from the grid 
     """
-    return (net.res_ext_grid.p_mw / max_discrepancy) ** 2
+    global max_discrepancy
+
+    return np.sum(net.res_ext_grid.p_mw / max_discrepancy) ** 2
 
 def ext_grid_prices(net):
     return abs(net.poly_cost[net.poly_cost["et"] == "ext_grid"]["cp1_eur_per_mw"].item() * net.res_ext_grid.at[0,"p_mw"])
@@ -196,22 +224,20 @@ def ext_grid_prices(net):
 def grid_component_prices(net):
     return net.poly_cost["cp0_eur"].sum()
 
-def timeseries_runner(net, **kwargs):
+def timeseries_runner(net, acc, **kwargs):
     """
     Wrapper function around the pandapower timeseries. 
     Runs each individial step in the timeseries and extracts the violation costs for each step
     """
-    global bus_violation_cost, line_violation_cost, ext_grid_penalty_cost, ext_grid_prices_cost
-
     dispatch_storage(net, strategy='battery_first')
     runpp(net, max_iteration=40)
 
     ts = net["_timestep"]
 
-    bus_violation_cost[ts] = bus_violations(net).item()
-    line_violation_cost[ts] = line_violations(net).item()
-    ext_grid_penalty_cost[ts] = grid_penalty(net).item()
-    ext_grid_prices_cost[ts] = ext_grid_prices(net).item()
+    acc["bus"] += bus_violations(net)
+    acc["line"] += line_violations(net)
+    acc["grid_penalty"] += grid_penalty(net)
+    acc["grid_price"] += ext_grid_prices(net)
 
 
 
@@ -220,85 +246,74 @@ def ga_evaluator(solution: Solution):
     Runs the net timeseries for all target trials: the two worst case days, and the typical day for each month
     """
 
-    global monthly_profiles, iteration_counter, net_stats
     total_cost = 0
-    iteration_counter += 1
 
     try:
-
         for month in range(1,13):
             total_cost += deploy_net_runner(solution, month=month)
         #total_cost += deploy_net_runner(solution, date=net_stats["Peak Surplus Date"])
         #total_cost += deploy_net_runner(solution, date=net_stats["Peak Deficit Date"])
     except Exception as e: 
         # break if the net does not converge on any step in the timeseries, and return the maximum penalty.
-        #print(e)
+        # print(e)
+        # print(traceback.format_exc())
         return 1e20
-
-
-    print(f'generation {math.floor(iteration_counter / 60) + 1}  iteration  {(iteration_counter % 60)}')
-    print(f'bus err: {sum(bus_violation_cost.values())}   line err: {sum(line_violation_cost.values())}  grid reliance penalty:  {sum(ext_grid_penalty_cost.values())}   grid prices: {sum(ext_grid_prices_cost.values())}   components: {grid_component_prices(net)}   total cost {total_cost}')    
-    
-     # add in the last remaining penalty for the cost of components required for this iteration's solution
-    total_cost += grid_component_prices(net)        
     return total_cost
 
 
 if __name__ == '__main__':
     continued_flag = "-cont" in sys.argv
-
+    
     net = init_run()
     net_stats = energy_analysis(net)
-    print(net_stats)
     max_discrepancy = net_stats["Peak Deficit"]
     monthly_profiles = average_day(net)
+    
+    problem_kwargs = {
+        "ga_evaluate": ga_evaluator,
+        "max_p_mw": abs(net_stats["Peak Deficit"])
+    }
 
-    algorithm = GA(pop_size=120, sampling=FloatRandomSampling())
-
-    start_pop = None
-
-    if continued_flag:
-        with open("checkpoint.pkl", "rb") as f:
-            ckpt = dill.load(f)
-
-        algorithm = ckpt["algorithm"]
-        algorithm.n_gen += 1
-        algorithm.termination = get_termination("n_gen", algorithm.n_gen + 5)
-        print(f'resuming at {algorithm.n_gen} generations ({algorithm.evaluator.n_eval} evals)')
-    else:
-        algorithm = GA(pop_size=120, sampling=FloatRandomSampling())
-        algorithm.termination = get_termination("n_gen", 2)
-    result = []
-
-    # if running as a Colab, execute the program using 8 processes
+    pool = None
+    # add the runner only when in Colab
     if "gdrive" in os.getcwd():
         n_processes = 8
         pool = multiprocessing.Pool(n_processes)
-        runner = StarmapParallelization(pool.starmap)
-
-        problem = GridPlanningProblem(ga_evaluate=ga_evaluator, max_p_mw=abs(net_stats["Peak Deficit"]), elementwise_runner=runner)
-        result = minimize(
-            problem, 
-            algorithm, 
-            verbose=True, 
-            termination=None, 
-            copy_algorithm=not continued_flag, 
-            save_history=True, 
-            callback=CheckpointCallback())
-    # otherwise, execute as only a single process 
-    else:
-        problem = GridPlanningProblem(ga_evaluate=ga_evaluator, max_p_mw=abs(net_stats["Peak Deficit"]))
-        result = minimize(
-            problem, 
-            algorithm, 
-            verbose=True, 
-            termination=None, 
-            copy_algorithm=not continued_flag, 
-            save_history=True,
-            callback=CheckpointCallback())
-
-    print(f'Oprimized result: {result.X}')
-
-    if "gdrive" in os.getcwd():     # close multiprocess pools
-        pool.close()
+        problem_kwargs["elementwise_runner"] = StarmapParallelization(pool.starmap)
     
+    # initialize problem with or without elementwise_runner as an arg, depending on environment
+    problem = GridPlanningProblem(**problem_kwargs)
+
+    # setup for continuation from checkpoint, if requested (script run with "-cont" as argument)
+    if continued_flag:
+        with open("checkpoint.pkl", "rb") as f:
+            ckpt = dill.load(f)
+        algorithm = ckpt["algorithm"]
+
+        algorithm.n_gen += 1
+        algorithm.termination = get_termination("n_gen", algorithm.n_gen + 5)
+        algorithm.problem = problem
+        if "gdrive" in os.getcwd(): algorithm.evaluator.elementwise_runner = problem.elementwise_runner
+
+        # re-link the algorithm to the fresh problem (and runner)
+        algorithm.setup(problem, progress=True, verbose=True, callback=CheckpointCallback())
+
+        print(f'loading checkpoint at generation {algorithm.n_gen} eval {algorithm.evaluator.n_eval}')
+    else:
+        algorithm = MixedVariableGA(pop_size=120)
+        algorithm.termination = get_termination("n_gen", 1)
+
+    # run Optimization
+    result = minimize(
+        problem,
+        algorithm,
+        verbose=True,
+        termination=None,
+        copy_algorithm=not continued_flag,
+        save_history=True,
+        callback=CheckpointCallback()
+    )
+
+    if pool is not None:
+        pool.close()
+        pool.join()
--- src/tools/ga/problem.py
+++ src/tools/ga/problem.py
@@ -1,76 +1,58 @@
 from pymoo.core.problem import ElementwiseProblem
+from pymoo.core.variable import Real, Integer, Binary
 from .solution import Solution
 import numpy as np
 import os
 
-class GridPlanningProblem(
-    ElementwiseProblem
-):
+class GridPlanningProblem(ElementwiseProblem):
 
     def __init__(self, ga_evaluate, max_p_mw, **kwargs):
 
+        self.vars = {
+            "batt_bus1": Integer(bounds=(0, 30)),
+            "batt_p_mw1": Real(bounds=(0.5, max_p_mw)),
+            "batt_e_mwh1": Real(bounds=(2.0, max_p_mw * 4)),
+
+            "batt_on2": Binary(), 
+            "batt_bus2": Integer(bounds=(0, 30)),
+            "batt_p_mw2": Real(bounds=(0.0, max_p_mw)),
+            "batt_e_mwh2": Real(bounds=(0.0, max_p_mw * 4)),
+
+            "batt_on3": Binary(),
+            "batt_bus3": Integer(bounds=(0, 29)),
+            "batt_p_mw3": Real(bounds=(0.0, max_p_mw)),
+            "batt_e_mwh3": Real(bounds=(0.0, max_p_mw * 4)),
+
+            "h2_bus1": Integer(bounds=(0, 30)),
+            "h2_num_electrolyzers1": Integer(bounds=(0, 10000)),
+            "h2_num_fuelcells1": Integer(bounds=(0, 10000)),
+            "h2_num_tanks1": Integer(bounds=(0, 10000)),
+
+            "h2_on2": Binary(),
+            "h2_bus2": Integer(bounds=(0, 30)),
+            "h2_num_electrolyzers2": Integer(bounds=(0, 10000)),
+            "h2_num_fuelcells2": Integer(bounds=(0, 10000)),
+            "h2_num_tanks2": Integer(bounds=(0, 10000))
+        }
+
         self.ga_evaluate = ga_evaluate
-
-        super().__init__(
-            n_var=20,
-            n_obj=1,
-            n_ieq_constr=20,
-            elementwise_evaluation=True if "gdrive" in os.getcwd() else False,
-            xl=[
-                0,                  # batt_bus1
-                0.5,                # batt_p_mw1
-                2,                  # batt_e_mwh1
-
-                0,                  # batt_on2
-                0,                  # batt_bus2
-                0  ,                # batt_p_mw2
-                0,                  # batt_e_mwh2
-
-                0,                  # batt_on3
-                0,                  # batt_bus3
-                0,                  # batt_p_mw3
-                0,                  # batt_e_mwh3
-
-                0,                  # h2_bus1
-                0,                  # h2_num_electrolyzers1
-                0,                  # h2_num_fuelcells1
-                0,                  # h2_num_tanks1
-
-                0,                  # h2_on2
-                0,                  # h2_bus2
-                0,                  # h2_num_electrolyzers2
-                0,                  # h2_num_fuelcells2
-                0                   # h2_num_tanks1
-            ],
-            xu=[
-                30,                 # batt_bus1
-                max_p_mw,           # batt_p_mw1
-                max_p_mw * 4,       # batt_e_mwh1
-
-                2,                  # batt_on2
-                30,                 # batt_bus2
-                max_p_mw,           # batt_p_mw2
-                max_p_mw * 4,       # batt_e_mwh2
-
-                2,                  # batt_on3
-                29,                 # batt_bus3
-                max_p_mw,           # batt_p_mw3
-                max_p_mw * 4,       # batt_e_mwh3
-
-                30,                 # h2_bus1
-                10000,              # h2_num_electrolyzers1
-                10000,              # h2_num_fuelcells1
-                10000,               # h2_num_tanks1
+        self.n_var = 20 
+        
+        """xl = []
+        xu = []
+        for name, var in vars.items():
+            if isinstance(var, Binary):
+                xl.append(0)
+                xu.append(1)
+            else:
+                xl.append(var.bounds[0])
+                xu.append(var.bounds[1])
+        
+        self.xl = np.array(xl)
+        self.xu = np.array(xu)"""
 
 
-                2,                  # h2_on2
-                30,                 # h2_bus2
-                10000,              # h2_num_electrolyzers2
-                10000,              # h2_num_fuelcells2
-                10000               # h2_num_tanks2
-            ],
-            **kwargs
-        )
+        super().__init__(vars, n_var=self.n_var, n_obj=1, n_ieq_constr=13, **kwargs)
 
     def _evaluate(
         self,
@@ -81,78 +63,78 @@ class GridPlanningProblem(
     ):
 
         sol = Solution(
-            batt_bus1=int(x[0]),
-            batt_p_mw1=x[1],
-            batt_e_mwh1=x[2],
+            batt_bus1=x["batt_bus1"],
+            batt_p_mw1=x["batt_p_mw1"],
+            batt_e_mwh1=x["batt_e_mwh1"],
 
-            batt_on2=int(x[3]),
-            batt_bus2=int(x[4]),
-            batt_p_mw2=x[5],
-            batt_e_mwh2=x[6],
+            batt_on2=x["batt_on2"],
+            batt_bus2=x["batt_bus2"],
+            batt_p_mw2=x["batt_p_mw2"],
+            batt_e_mwh2=x["batt_e_mwh2"],
 
-            batt_on3=int(x[7]),
-            batt_bus3=int(x[8]),
-            batt_p_mw3=x[9],
-            batt_e_mwh3=x[10],
+            batt_on3=x["batt_on3"],
+            batt_bus3=x["batt_bus3"],
+            batt_p_mw3=x["batt_p_mw3"],
+            batt_e_mwh3=x["batt_e_mwh3"],
 
-            h2_bus1=int(x[11]),
-            h2_num_electrolyzers1=int(x[12]),
-            h2_num_fuelcells1=int(x[13]),
-            h2_num_tanks1=int(x[14]),
+            h2_bus1=x["h2_bus1"],
+            h2_num_electrolyzers1=x["h2_num_electrolyzers1"],
+            h2_num_fuelcells1=x["h2_num_fuelcells1"],
+            h2_num_tanks1=x["h2_num_tanks1"],
 
-            h2_on2=int(x[15]),
-            h2_bus2=int(x[16]),
-            h2_num_electrolyzers2=int(x[17]),
-            h2_num_fuelcells2=int(x[18]),
-            h2_num_tanks2=int(x[19]),
+            h2_on2=x["h2_on2"],
+            h2_bus2=x["h2_bus2"],
+            h2_num_electrolyzers2=x["h2_num_electrolyzers2"],
+            h2_num_fuelcells2=x["h2_num_fuelcells2"],
+            h2_num_tanks2=x["h2_num_tanks2"]
         )
 
         out["F"] = np.sum(self.ga_evaluate(sol))
 
         # batteries must have at least 4 hours' capacity
-        g1 = 4 * x[1] - x[2]
-        g2 = 0 if int(x[3]) == 0 else 4 * x[5] - x[6]
-        g3 = 0 if int(x[7]) == 0 else 4 * x[9] - x[10]
+        g1 = 4 * x["batt_p_mw1"] - x["batt_e_mwh1"]
+        g2 = 0 if x["batt_on2"] == 0 else 4 * x["batt_p_mw2"] - x["batt_e_mwh2"]
+        g3 = 0 if x["batt_on3"] == 0 else 4 * x["batt_p_mw3"] - x["batt_e_mwh3"]
 
-        b1 = int(x[0])
-        b2 = int(x[4])
-        b3 = int(x[8])
-        h1 = int(x[11])
-        h2 = int(x[16])
+        b1 = x["batt_bus1"]
+        b2 = x["batt_bus2"]
+        b3 = x["batt_bus3"]
+        h1 = x["h2_bus1"]
+        h2 = x["h2_bus2"]
 
-        # Only enforce bus uniqueness for enabled assets
-        c12 = 0 if int(x[3]) == 0 else 1 - abs(b1 - b2)
-        c13 = 0 if int(x[7]) == 0 else 1 - abs(b1 - b3)
-        c23 = 0 if int(x[3]) == 0 or int(x[7]) == 0 else 1 - abs(b2 - b3)
+        # enforce bus uniqueness for enabled assets
+        c12 = 0 if x["batt_on2"] == 0 else 1 - abs(b1 - b2)
+        c13 = 0 if x["batt_on3"] == 0 else 1 - abs(b1 - b3)
+        c23 = 0 if x["batt_on2"] == 0 or x["batt_on3"] == 0 else 1 - abs(b2 - b3)
 
         c1h1 = 1 - abs(b1 - h1)
-        c2h1 = 0 if int(x[3]) == 0 else 1 - abs(b2 - h1)
-        c3h1 = 0 if int(x[7]) == 0 else 1 - abs(b3 - h1)
+        c2h1 = 0 if x["batt_on2"] == 0 else 1 - abs(b2 - h1)
+        c3h1 = 0 if x["batt_on3"] == 0 else 1 - abs(b3 - h1)
 
-        c1h2 = 0 if int(x[15]) == 0 else 1 - abs(b1 - h2)
-        c2h2 = 0 if int(x[3]) == 0 or int(x[15]) == 0 else 1 - abs(b2 - h2)
-        c3h2 = 0 if int(x[7]) == 0 or int(x[15]) == 0 else 1 - abs(b3 - h2)
+        c1h2 = 0 if x["h2_on2"] == 0 else 1 - abs(b1 - h2)
+        c2h2 = 0 if x["batt_on2"] == 0 or x["h2_on2"] == 0 else 1 - abs(b2 - h2)
+        c3h2 = 0 if x["batt_on3"] == 0 or x["h2_on2"] == 0 else 1 - abs(b3 - h2)
 
-        ch1h2 = 0 if int(x[15]) == 0 else 1 - abs(h1 - h2)
+        ch1h2 = 0 if x["h2_on2"] == 0 else 1 - abs(h1 - h2)
     
-        g_batt2_p = 0 if int(x[3]) != 0 else abs(x[5])
-        g_batt2_e = 0 if int(x[3]) != 0 else abs(x[6])
-        g_batt3_p = 0 if int(x[7]) != 0 else abs(x[9])
-        g_batt3_e = 0 if int(x[7]) != 0 else abs(x[10])
+        # g_batt2_p = 0 if x["batt_on2"] != 0 else abs(x[5])
+        # g_batt2_e = 0 if x["batt_on2"] != 0 else abs(x[6])
+        # g_batt3_p = 0 if x["batt_on3"] != 0 else abs(x[9])
+        # g_batt3_e = 0 if x["batt_on3"] != 0 else abs(x[10])
 
-        g_h2_elec = 0 if int(x[15]) != 0 else x[17]
-        g_h2_fc   = 0 if int(x[15]) != 0 else x[18]
-        g_h2_t   = 0 if int(x[15]) != 0 else x[19]
+        # g_h2_elec = 0 if x["h2_on2"] != 0 else x[17]
+        # g_h2_fc   = 0 if x["h2_on2"] != 0 else x[18]
+        # g_h2_t   = 0 if x["h2_on2"] != 0 else x[19]
 
         out["G"] = [
             g1, g2, g3,
             c12, c13, c23,
             c1h1, c2h1, c3h1,
             c1h2, c2h2, c3h2,
-            ch1h2, 
-            g_batt2_p, g_batt2_e,
-            g_batt3_p, g_batt3_e,
-            g_h2_elec, g_h2_fc, g_h2_t
+            ch1h2# , 
+            # g_batt2_p, g_batt2_e,
+            # g_batt3_p, g_batt3_e,
+            # g_h2_elec, g_h2_fc, g_h2_t
         ]
 
         print(f'{out["F"]}:   {sol}')
